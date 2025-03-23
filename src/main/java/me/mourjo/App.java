a9009b18441e0b37db83b1c775a6569431389114
Mourjo Sen <hello@mourjo.me>
2025-03-23 12:45:03 +0100

    Add Github workflow

--- src/main/java/me/mourjo/App.java
+++ src/main/java/me/mourjo/App.java
@@ -1,13 +1,13 @@
 package me.mourjo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
+
+    public static String saySomething() {
+        return "Hello, world!";
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( saySomething());
     }
 }
