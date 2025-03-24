140341ac827027f0bc8cb25489f205bb559904e5
Mourjo Sen <hello@mourjo.me>
2025-03-24 06:26:38 +0100

    Meetings schema

---
+++ src/main/java/me/mourjo/common/Database.java
@@ -0,0 +1,18 @@
+package me.mourjo.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import lombok.SneakyThrows;
+
+public class Database {
+
+    @SneakyThrows
+    public static Connection getConnection() {
+        String host = Environment.getPostgresHost();
+        String port = Environment.getPostgresPort();
+        String database = Environment.getPostgresDatabase();
+        String username = Environment.getPostgresUser();
+        String connectionString = "jdbc:postgresql://%s:%s/%s".formatted(host, port, database);
+        return DriverManager.getConnection(connectionString, username, null);
+    }
+}
--- src/main/java/me/mourjo/entities/Meeting.java
+++ src/main/java/me/mourjo/entities/Meeting.java
@@ -12,5 +12,61 @@ import org.jooq.impl.DSL;
 @Getter
 @Table(name = "meetings")
 public class Meeting {
+    @Column(name="id")
+    long id;
+
+    @Column(name="name")
+    String name;
+
+    @Column(name = "start_at")
+    OffsetDateTime startTime;
+
+    @Column(name = "end_at")
+    OffsetDateTime endTime;
+
+    @Column(name = "created_at")
+    OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    OffsetDateTime updatedAt;
+
+
+    public static Field<Long> idField() {
+        return DSL.field("id", Long.class);
+    }
+
+    public static Field<String> nameField() {
+        return DSL.field("name", String.class);
+    }
+
+    public static Field<OffsetDateTime> startTimeField() {
+        return DSL.field("start_at", OffsetDateTime.class);
+    }
+
+    public static Field<OffsetDateTime> endTimeField() {
+        return DSL.field("end_at", OffsetDateTime.class);
+    }
+
+    public static Field<OffsetDateTime> createdAtField() {
+        return DSL.field("created_at", OffsetDateTime.class);
+    }
+    public static Field<OffsetDateTime> updatedAtField() {
+        return DSL.field("updated_at", OffsetDateTime.class);
+    }
+
+    public static org.jooq.Table<org.jooq.Record> table() {
+        return DSL.table("meetings");
+    }
+
+    public static SelectFieldOrAsterisk[] asterisk() {
+        return new SelectFieldOrAsterisk[]{
+            idField(),
+            nameField(),
+            startTimeField(),
+            endTimeField(),
+            createdAtField(),
+            updatedAtField(),
+        };
+    }
 
 }
