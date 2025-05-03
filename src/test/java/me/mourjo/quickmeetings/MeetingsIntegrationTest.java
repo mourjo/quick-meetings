f27989e13207b38851953a6e28866fe60697d74e
Mourjo Sen <hello@mourjo.me>
2025-05-03 13:51:21 +0200

    Use AssertJ

--- src/test/java/me/mourjo/quickmeetings/MeetingsIntegrationTest.java
+++ src/test/java/me/mourjo/quickmeetings/MeetingsIntegrationTest.java
@@ -1,5 +1,7 @@
 package me.mourjo.quickmeetings;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.OffsetDateTime;
 import me.mourjo.quickmeetings.db.Meeting;
 import me.mourjo.quickmeetings.db.MeetingRepository;
@@ -9,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QuickmeetingsApplicationTests {
+class MeetingsIntegrationTest {
 
     @Autowired
     MeetingRepository repository;
@@ -21,30 +23,33 @@ class QuickmeetingsApplicationTests {
             .endAt(OffsetDateTime.now().plusHours(1))
             .name("Test meeting")
             .build();
-        var m = repository.save(meeting);
+        var savedMeeting = repository.save(meeting);
 
         boolean found = false;
         for (var dbMeeting : repository.findAll()) {
-            if (dbMeeting.id() == m.id()) {
+            if (dbMeeting.id() == savedMeeting.id()) {
                 found = true;
+                assertThat(dbMeeting.name()).isEqualTo(meeting.name());
 
-                Assertions.assertEquals(meeting.name(), dbMeeting.name());
-                Assertions.assertEquals(meeting.startAt().toEpochSecond(),
-                    dbMeeting.startAt().toEpochSecond());
-                Assertions.assertEquals(meeting.endAt().toEpochSecond(),
-                    dbMeeting.endAt().toEpochSecond());
+                assertThat(dbMeeting.startAt().toEpochSecond())
+                    .isEqualTo(meeting.startAt().toEpochSecond());
+
+                assertThat(dbMeeting.endAt().toEpochSecond())
+                    .isEqualTo(meeting.endAt().toEpochSecond());
 
                 Assertions.assertTrue(OffsetDateTime.now().isAfter(dbMeeting.createdAt()));
                 Assertions.assertTrue(OffsetDateTime.now().isAfter(dbMeeting.updatedAt()));
             }
         }
 
-        Assertions.assertTrue(found);
+        assertThat(found).isTrue();
 
-        var dbMeeting = repository.findById(m.id()).get();
+        var dbMeeting = repository.findById(savedMeeting.id()).get();
         repository.save(Meeting.buildFrom(dbMeeting).name("New Name").build());
 
-        Assertions.assertEquals("New Name", repository.findById(m.id()).get().name());
+        assertThat(
+            repository.findById(savedMeeting.id()).get().name()
+        ).isEqualTo("New Name");
     }
 
 }
