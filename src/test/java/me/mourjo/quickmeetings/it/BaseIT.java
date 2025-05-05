b9c811b94dcef9495269868e117d1540b59084f1
Mourjo Sen <hello@mourjo.me>
2025-05-05 12:26:20 +0200

    Do not purge database after test

--- src/test/java/me/mourjo/quickmeetings/it/BaseIT.java
+++ src/test/java/me/mourjo/quickmeetings/it/BaseIT.java
@@ -14,9 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public abstract class BaseIT {
@@ -47,14 +45,14 @@ public abstract class BaseIT {
 
     @AfterEach
     void teardown() {
-        userMeetingRepository.deleteAll();
-        userRepository.deleteAll();
-        meetingRepository.deleteAll();
     }
 
 
     @BeforeEach
     void setup() {
+        userMeetingRepository.deleteAll();
+        userRepository.deleteAll();
+        meetingRepository.deleteAll();
         meetingUtils = new MeetingUtils(
             userService, meetingsService, meetingRepository, userRepository, userMeetingRepository
         );
