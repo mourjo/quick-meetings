12bbf002c9b72177ac0fad245c257953bd80f5f7
Mourjo Sen <hello@mourjo.me>
2025-05-05 12:02:24 +0200

    BaseIT should be transactional

--- src/test/java/me/mourjo/quickmeetings/it/BaseIT.java
+++ src/test/java/me/mourjo/quickmeetings/it/BaseIT.java
@@ -14,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public abstract class BaseIT {
