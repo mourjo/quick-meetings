2d19eacbdc1835feb8f67305ea7bc8ca31b21b24
Mourjo Sen <hello@mourjo.me>
2025-05-18 11:05:54 +0200

    Use TestRestTemplate

---
+++ src/main/java/me/mourjo/quickmeetings/web/WebConfiguration.java
@@ -0,0 +1,17 @@
+package me.mourjo.quickmeetings.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+            .ignoreAcceptHeader(true)
+            .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
--- src/test/java/me/mourjo/quickmeetings/generativetests/RequestResponseGenTests.java
+++ src/test/java/me/mourjo/quickmeetings/generativetests/RequestResponseGenTests.java
@@ -3,21 +3,21 @@ package me.mourjo.quickmeetings.generativetests;
 import static me.mourjo.quickmeetings.utils.RequestUtils.MEETING_CREATION_BODY_JSON;
 import static me.mourjo.quickmeetings.utils.RequestUtils.MEETING_INVITE_BODY;
 import static me.mourjo.quickmeetings.utils.RequestUtils.MEETING_INVITE_UPDATE_BODY_JSON;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import java.time.ZoneId;
 import java.util.Map;
-import java.util.UnknownFormatConversionException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import me.mourjo.quickmeetings.db.MeetingRepository;
 import me.mourjo.quickmeetings.db.User;
 import me.mourjo.quickmeetings.db.UserMeetingRepository;
 import me.mourjo.quickmeetings.db.UserRepository;
 import me.mourjo.quickmeetings.service.UserService;
+import net.jqwik.api.AfterFailureMode;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.Combinators;
@@ -29,55 +29,41 @@ import net.jqwik.api.lifecycle.AfterProperty;
 import net.jqwik.api.lifecycle.BeforeProperty;
 import net.jqwik.spring.JqwikSpringSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @JqwikSpringSupport
 public class RequestResponseGenTests {
 
-    @Autowired
-    MockMvc mockMvc;
-
-
     User alice;
-    ObjectMapper om = JsonMapper.builder()
+
+    ObjectMapper objectMapper = JsonMapper.builder()
         .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
         .build();
 
-    private static MockHttpServletRequestBuilder buildRequest(String path,
-        String contentType, String name, long userId, String fromDate, String fromTime,
-        String toDate, String toTime,
-        String timezone, String method, String body) {
-        MockHttpServletRequestBuilder req = switch (method) {
-            case "GET" -> MockMvcRequestBuilders.get(path);
-            case "POST" -> MockMvcRequestBuilders.post(path);
-            case "PUT" -> MockMvcRequestBuilders.put(path);
-            case "DELETE" -> MockMvcRequestBuilders.delete(path);
-            default -> throw new IllegalStateException("Unexpected value: " + method);
-        };
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-        try {
-            var formattedBody = body.formatted(
-                userId,
-                name,
-                fromDate,
-                fromTime,
-                toDate,
-                toTime,
-                timezone
-            ).replaceAll("%", "");
-            return req.content(formattedBody)
-                .contentType(contentType);
+    @LocalServerPort
+    private int port;
 
-        } catch (UnknownFormatConversionException ex) {
-            return req.content(body.replaceAll("%", "")).contentType(contentType);
-        }
+    public ResponseEntity<String> postRequest(String uri, String body, HttpHeaders headers) {
+        return requestBuilder(uri, body, headers).apply(HttpMethod.POST);
+    }
+
+    public ResponseEntity<String> putRequest(String uri, String body, HttpHeaders headers) {
+        return requestBuilder(uri, body, headers).apply(HttpMethod.PUT);
+    }
+
+    public ResponseEntity<String> deleteRequest(String uri, String body, HttpHeaders headers) {
+        return requestBuilder(uri, body, headers).apply(HttpMethod.DELETE);
     }
 
     @Provide
@@ -90,55 +76,43 @@ public class RequestResponseGenTests {
 
     @Provide
     Arbitrary<String> dates() {
-        return Arbitraries.frequencyOf(
-            Tuple.of(10, Arbitraries.of("2025-06-09", "2025-06-10", "2025-06-11", "2025-02-30")),
-            Tuple.of(1, Arbitraries.strings())
-        );
+        return Arbitraries.of("2025-06-09", "2025-06-10", "2025-06-11", "2025-02-30", "1-2-3-4");
     }
 
     @Provide
     Arbitrary<String> times() {
-        return Arbitraries.frequencyOf(
-            Tuple.of(10, Arbitraries.of("12:40", "01:10", "13:50", "23:30", "99:99")),
-            Tuple.of(1, Arbitraries.strings())
-        );
+        return Arbitraries.of("12:40", "01:10", "13:50", "23:30", "99:99", "0-9-8-7");
     }
 
     @Provide
     Arbitrary<String> timezones() {
-        return Arbitraries.frequencyOf(
-            Tuple.of(3, Arbitraries.of("Asia/Kolkata"),
-                Tuple.of(3, Arbitraries.of(ZoneId.getAvailableZoneIds())),
-                Tuple.of(1, Arbitraries.strings())
-            ));
+        return Arbitraries.of("Asia/Kolkata", "Etc/Utc", "thisisaninvalidtimezone");
     }
 
-    @Property
+    @SneakyThrows
+    void assertThatValidJson(String string, String msg) {
+        assertThat(objectMapper.readValue(string, Map.class))
+            .withFailMessage(msg)
+            .isInstanceOf(Map.class);
+    }
+
+    @Property(afterFailure = AfterFailureMode.RANDOM_SEED)
     @SneakyThrows
     void responsesAreAlwaysValidJson(
         @ForAll("methods") String method,
         @ForAll("paths") String path,
-        @ForAll("contentTypes") String contentType,
-        @ForAll("bodies") String body,
-        @ForAll String name,
-        @ForAll("userIds") Long userId,
-        @ForAll("dates") String fromDate,
-        @ForAll("times") String fromTime,
-        @ForAll("dates") String toDate,
-        @ForAll("times") String toTime,
-        @ForAll("timezones") String timezone) {
+        @ForAll("contentTypes") String contentTypeHeader,
+        @ForAll("contentTypes") String acceptHeader,
+        @ForAll("bodies") String body) {
 
-        MockHttpServletRequestBuilder req = buildRequest(
-            path, contentType, name, userId, fromDate, fromTime, toDate, toTime, timezone, method,
-            body);
+        var response = httpRequest(method, path, acceptHeader, contentTypeHeader, body);
+        var responseBody = response.getBody();
+        int status = response.getStatusCode().value();
 
-        mockMvc.perform(req).andDo(res -> {
-            assertThat(res.getResponse().getStatus()).isLessThan(500);
-            assertThat(res.getResponse().getContentType()).isEqualTo(APPLICATION_JSON_VALUE);
-            var responseBody = res.getResponse().getContentAsString();
-
-            assertThat(om.readValue(responseBody, Map.class)).isInstanceOf(Map.class);
-        });
+        var failureMsg = "Status: %s, Body: %s".formatted(status, responseBody);
+        assertThat(responseBody).withFailMessage(failureMsg).isNotBlank();
+        assertThatValidJson(responseBody, failureMsg);
+        assertThat(status).withFailMessage(failureMsg).isLessThan(500);
     }
 
     @BeforeProperty
@@ -157,42 +131,14 @@ public class RequestResponseGenTests {
 
     @Provide
     Arbitrary<String> contentTypes() {
-        return Arbitraries.oneOf(
-            Arbitraries.strings().alpha(),
-            Arbitraries.of(
-                MediaType.ALL_VALUE,
-                MediaType.APPLICATION_ATOM_XML_VALUE,
-                MediaType.APPLICATION_CBOR_VALUE,
-                MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-                MediaType.APPLICATION_GRAPHQL_RESPONSE_VALUE,
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_OCTET_STREAM_VALUE,
-                MediaType.APPLICATION_PDF_VALUE,
-                MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-                MediaType.APPLICATION_PROBLEM_XML_VALUE,
-                MediaType.APPLICATION_PROTOBUF_VALUE,
-                MediaType.APPLICATION_RSS_XML_VALUE,
-                MediaType.APPLICATION_NDJSON_VALUE,
-                MediaType.APPLICATION_XHTML_XML_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE,
-                MediaType.IMAGE_GIF_VALUE,
-                MediaType.IMAGE_JPEG_VALUE,
-                MediaType.IMAGE_PNG_VALUE,
-                MediaType.MULTIPART_FORM_DATA_VALUE,
-                MediaType.MULTIPART_MIXED_VALUE,
-                MediaType.MULTIPART_RELATED_VALUE,
-                MediaType.TEXT_EVENT_STREAM_VALUE,
-                MediaType.TEXT_HTML_VALUE,
-                MediaType.TEXT_MARKDOWN_VALUE,
-                MediaType.TEXT_PLAIN_VALUE,
-                MediaType.TEXT_XML_VALUE)
-        );
+        return Arbitraries.of(
+            MediaType.TEXT_HTML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE);
     }
 
     @Provide
     Arbitrary<String> paths() {
-        return Arbitraries.oneOf(Arbitraries.of(
+        return Arbitraries.of(
             "/user",
             "/meeting",
             "/meeting/invite",
@@ -200,28 +146,108 @@ public class RequestResponseGenTests {
             "/meeting/reject",
             "/non-existent",
             "/"
-        ), Arbitraries.strings().alpha()).map(s -> s.startsWith("/") ? s : "/" + s);
+        );
     }
 
     @Provide
     Arbitrary<String> bodies() {
         return Arbitraries.oneOf(
-            Arbitraries.strings(),
-            Combinators.combine(Arbitraries.integers(), Arbitraries.integers())
-                .as("[%s,%s]"::formatted),
-            Arbitraries.of(
-                MEETING_CREATION_BODY_JSON,
-                MEETING_INVITE_BODY,
-                MEETING_INVITE_UPDATE_BODY_JSON,
-                "this is random",
-                ""
-            ));
+            meetingCreationRequest(),
+            meetingInviteRequest(),
+            updateInviteRequest(),
+            Arbitraries.strings()
+        );
     }
 
     @Provide
     Arbitrary<String> methods() {
         return Arbitraries.of(
             "GET", "POST", "PUT", "DELETE"
+        );
+    }
+
+    Arbitrary<String> meetingCreationRequest() {
+        return Combinators.combine(
+                Arbitraries.of(MEETING_CREATION_BODY_JSON),
+                Arbitraries.integers().between(1, 10),
+                Arbitraries.strings().alpha().ofMinLength(1).ofMaxLength(5),
+                dates(),
+                times(),
+                dates(),
+                times(),
+                timezones()
+            ).as(Tuple::of)
+            .map(t ->
+                t.get1().formatted(
+                    t.get2(),
+                    t.get3(),
+                    t.get4(),
+                    t.get5(),
+                    t.get6(),
+                    t.get7(),
+                    t.get8()
+                )
+            );
+    }
+
+    Arbitrary<String> meetingInviteRequest() {
+        return Combinators.combine(
+                Arbitraries.of(MEETING_INVITE_BODY), Arbitraries.integers().greaterOrEqual(1),
+                Arbitraries.integers().greaterOrEqual(1).list()
+            ).as(Tuple::of)
+            .map(t ->
+                t.get1().formatted(
+                    t.get2(),
+                    t.get3().stream().map(String::valueOf).collect(Collectors.joining(","))
+                )
+            );
+    }
+
+    Arbitrary<String> updateInviteRequest() {
+        return Combinators.combine(
+                Arbitraries.of(MEETING_INVITE_UPDATE_BODY_JSON),
+                Arbitraries.integers().greaterOrEqual(1),
+                Arbitraries.integers().greaterOrEqual(1)
+            ).as(Tuple::of)
+            .map(t ->
+                t.get1().formatted(
+                    t.get2(),
+                    t.get3()
+                )
+            );
+    }
+
+    ResponseEntity<String> httpRequest(String method, String path, String acceptHeader,
+        String contentTypeHeader, String body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, acceptHeader);
+        headers.add(HttpHeaders.CONTENT_TYPE, contentTypeHeader);
+
+        return switch (method) {
+            case "GET" -> getRequest(path, headers);
+            case "POST" -> postRequest(path, body, headers);
+            case "PUT" -> putRequest(path, body, headers);
+            case "DELETE" -> deleteRequest(path, body, headers);
+            default -> throw new IllegalArgumentException("Unexpected method");
+        };
+    }
+
+    ResponseEntity<String> getRequest(String uri, HttpHeaders headers) {
+        return testRestTemplate.exchange(
+            uri,
+            HttpMethod.GET,
+            new HttpEntity<>(headers),
+            String.class
+        );
+    }
+
+    Function<HttpMethod, ResponseEntity<String>> requestBuilder(String uri, String body,
+        HttpHeaders headers) {
+        return method -> testRestTemplate.exchange(
+            uri,
+            method,
+            new HttpEntity<>(body, headers),
+            String.class
         );
     }
 }
