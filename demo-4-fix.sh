d470e6e93142f8822cf6bfc67593861d207def61
Mourjo Sen <hello@mourjo.me>
2025-05-25 10:43:53 +0200

    Remove all fixes

--- demo-4-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-current_branch=$(git rev-parse --abbrev-ref HEAD) ;
-
-if [ "$current_branch" = "demo-4-meeting-acceptations" ]; then
-  git revert --no-commit 6910be3 && git reset HEAD
-else
-  echo "Current branch is not demo-4-meeting-acceptations"
-fi
