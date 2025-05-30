f531e91ed364f97b3a68d4305b5a3ef3b04aa46e
Mourjo Sen <hello@mourjo.me>
2025-05-30 09:18:08 +0200

    Clean after switching branches

--- fix.sh
+++ fix.sh
@@ -4,6 +4,7 @@ current_branch=$(git rev-parse --abbrev-ref HEAD) ;
 
 if [ "$current_branch" = "demo-5-empty-meetings" ]; then
   git revert --no-commit 42db256 && git reset HEAD
+  mvn clean
 else
   echo "Current branch is not demo-5-empty-meetings"
 fi
