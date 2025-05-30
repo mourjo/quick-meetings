0ae0a2e21986959de5f129dc729b0d0d6bf42de5
Mourjo Sen <hello@mourjo.me>
2025-05-30 09:10:15 +0200

    Clean after switching branches

--- fix.sh
+++ fix.sh
@@ -5,6 +5,7 @@ current_branch=$(git rev-parse --abbrev-ref HEAD) ;
 
 if [ "$current_branch" = "demo-2-invalid-date-range" ]; then
   git revert --no-commit 20ac61b && git reset HEAD
+  mvn clean
 else
   echo "Current branch is not demo-2-invalid-date-range"
 fi
