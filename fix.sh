1fbd08d6f3d48fc264ce6209e8eca303485f4c1f
Mourjo Sen <hello@mourjo.me>
2025-08-17 10:42:39 +0200

    Update workflow to run tests after fix application

--- fix.sh
+++ fix.sh
@@ -4,7 +4,7 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD) ;
 
 if [ "$current_branch" = "demo-2-invalid-date-range" ]; then
-  git revert --no-commit 20ac61b && git reset HEAD
+  git revert --no-commit 20ac61bee2473dc1c663321e28ea7f61bf428f2b && git reset HEAD
   mvn clean
 else
   echo "Current branch is not demo-2-invalid-date-range"
