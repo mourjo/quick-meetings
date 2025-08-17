c0112ad7a1011710317ea33bbccd1adba71b5c76
Mourjo Sen <hello@mourjo.me>
2025-08-17 10:45:39 +0200

    Update workflow to run tests after fix application

--- fix.sh
+++ fix.sh
@@ -4,7 +4,7 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD) ;
 
 if [ "$current_branch" = "demo-3-meeting-creation-scenarios" ]; then
-  git revert --no-commit 7cb6fc9 && git reset HEAD
+  git revert --no-commit 7cb6fc948a49281e73dedfdf2899b3a04d8f34a9 && git reset HEAD
   mvn clean
 else
   echo "Current branch is not demo-3-meeting-creation-scenarios"
